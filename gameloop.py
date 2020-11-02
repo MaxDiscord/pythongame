1ed394504bfaa95a6321172aeaeaae08edc420f2
Slap Slap <kaibergman22@gmail.com>
2020-11-02 01:25:32 +0000

    old code

---
+++ gameloop.py
@@ -0,0 +1,8 @@
+import maps
+playing = True
+class Gameloop:
+	def gameloop():
+
+		while playing == True:
+			main = input('> ')
+			print(maps.playerLocation)
---
+++ player.py
@@ -0,0 +1,4 @@
+import maps
+class Player:
+	location = [maps.x_size /2, maps.y_size/2]
+	print (location)
